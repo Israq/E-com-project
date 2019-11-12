152741d6bfdfd516e19b7a472557496c1a0dec8c
Syed Ragib Israq <35467653+Israq@users.noreply.github.com>
2019-11-12 20:45:30 +0600

    Add files via upload

--- products/models.py
+++ products/models.py
@@ -1,10 +1,12 @@
 import random
 import os
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
 
 
-from .utils import unique_slug_generator
+from cfehome.utils import unique_slug_generator
 
 # Create your models here.
 def get_filename_ext(filepath):
@@ -29,6 +31,14 @@ class ProductQuerySet(models.query.QuerySet):
     def featured(self):
         return self.filter(featured=True, active=True)
 
+    def search(self, query):
+        lookups = (Q(title__icontains=query) 
+                |  Q(description__icontains=query)
+                |  Q(price__icontains=query)
+                |  Q(tag__title__icontains=query))
+        return self.filter(lookups).distinct()
+            
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
@@ -45,6 +55,9 @@ class ProductManager(models.Manager):
             return qs.first()
         return None
 
+    def search(self, query):
+        
+        return self.get_queryset().active().search(query)
                          
 class Product(models.Model):
     title       = models.CharField(max_length=120)
@@ -54,12 +67,14 @@ class Product(models.Model):
     image       = models.ImageField(upload_to=upload_image_path,null=True,blank=True)
     featured    = models.BooleanField(default=False)
     active      = models.BooleanField(default=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
     objects = ProductManager()
 
     
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
+        #return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail",kwargs={"slug":self.slug})
 
 
     def __str__(self):
@@ -68,6 +83,10 @@ class Product(models.Model):
     
     def __unicode__(self):
         return self.title
+
+    @property
+    def name(self):
+        return self.title      
 
 
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
